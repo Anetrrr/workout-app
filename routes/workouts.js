5616534aaf20a0e91ab972ca458f4b1c44c3856d
Anetor Omo-Ataga <anetor.ataga@gmail.com>
2023-01-13 00:24:26 +0100

    backend files

---
+++ routes/workouts.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {
+	createWorkout,
+	getWorkouts,
+	getWorkout,
+	deleteWorkout,
+	updateWorkout,
+} = require('../controllers/workoutController');
+
+const router = express.Router();
+
+// GET all workouts
+router.get('/', getWorkouts);
+
+// GET a single workouts
+router.get('/:id', getWorkout);
+// POST a new workouts
+
+router.post('/', createWorkout);
+
+// DELETE a  workout
+router.delete('/:id', deleteWorkout);
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout);
+
+module.exports = router;
